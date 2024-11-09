a89698190fc59765a703c2af72e44293a93177dc
oPaulin <pauloferadosgames@gmail.com>
2024-11-09 13:36:03 -0300

    ajustando a porta

--- appserver.js
+++ appserver.js
@@ -1,28 +1,31 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 3000;
+const port = process.env.PORT || 3000; // Para Glitch, usamos a variável de ambiente PORT
 app.use(cors());
-var data = [0,0,0,0,0]
+
+var data = [0, 0, 0, 0, 0];
 app.use(express.json());
 
 // Rota para receber dados do ESP32
 app.post('/data', (req, res) => {
   const sensorData = req.body;
-  if(data.length ==5){
-    data.shift()
+  if (data.length == 5) {
+    data.shift();
   }
   data.push(sensorData.sensorValue);
   console.log("Dados recebidos do ESP32:", sensorData);
-  
+
   // Responde com uma mensagem de sucesso
   res.status(200).send({ message: "Dados recebidos com sucesso!" });
 });
 
-app.get('/getData',(req,res)=>{
-  var d = {valor:data}
-  res.json(d)
-})
+// Rota para enviar os dados
+app.get('/getData', (req, res) => {
+  var d = { valor: data };
+  res.json(d);
+});
+
 // Inicia o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
