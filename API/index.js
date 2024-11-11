681e5c74cab0dffe6c1c4c0ebc7238bc26dbcaa9
oPaulin <pauloferadosgames@gmail.com>
2024-11-11 17:10:27 -0300

    serverp

--- API/index.js
+++ API/index.js
@@ -10,28 +10,25 @@ var dataTempDoAr = []
 app.use(express.json());
 
 // Rota para receber dados do Arduino
-app.post('/data2',(req,res)=>{
-  const sensorData = req.body;
-  if (dataUmidadeDoAr.length == 10) {
-    dataUmidadeDoAr.shift();
-  }
-  if (dataTempDoAr.length == 10) {
-    dataTempDoAr.shift();
-  } 
-  dataUmidadeDoAr.push(sensorData.umidAr)
-  dataTempDoAr.push(sensorData.tempAr)
-  // Responde com uma mensagem de sucesso
-  res.status(200).send({ message: "Dados recebidos com sucesso!" });
-})
+
 // Rota para receber dados do ESP32
 app.post('/data', (req, res) => {
   const sensorData = req.body;
   if (data.length == 10) {
     data.shift();
   } 
-if(sensorData.sensorValue <2600){
-  sensorData.sensorValue-=400;
-}
+  if (dataUmidadeDoAr.length == 10) {
+    dataUmidadeDoAr.shift();
+  }
+  if (dataTempDoAr.length == 10) {
+    dataTempDoAr.shift();
+  } 
+
+  if(sensorData.sensorValue <2600){
+    sensorData.sensorValue-=400;
+  }
+  dataUmidadeDoAr.push(sensorData.umidAr)
+  dataTempDoAr.push(sensorData.tempAr)
   data.push(parseInt((100-(sensorData.sensorValue/4095)*100)));
   console.log("Dados recebidos do ESP32:", sensorData);
 
